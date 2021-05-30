4be3fd4636b991007e54b4dca151da6ab81876d8
AdrianWap <mapadrianpetre@gmail.com>
2021-05-30 20:54:13 +0300

    final

--- index.php
+++ index.php
@@ -5,85 +5,60 @@
 		<meta charset="utf-8">
 		<title> Cross Species Workflow Web Server </title>
 		<link href="styles/style.css" rel="stylesheet" type="text/css">
-		<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+		<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
 		<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.1/css/all.css">
 	</head>
 
 
 	<body>
 
-		<div class="banner">
-			<img src="img/logo.png"></img>
-		</div>
+	<div class="topbar">
+		<nav class="navbar navbar-expand-sm bg-primary navbar-dark fixed-top">
+			
+			<ul class="navbar-nav">
+				<li class="nav-item">
+					<a class="nav-link active" href="index.php"><i class="fas fa-home"></i> Home</a>
+				</li>
+				<li class="nav-item">
+					<a class="nav-link" href="about.php"><i class="fas fa-info-circle"></i> About</a>
+				</li>
+				<li class="nav-item">
+					<a class="nav-link" href="help.php"><i class="fas fa-question-circle"></i> Help</a>
+				</li>
+				<li class="nav-item">
+					<a class="nav-link" href="contact.php"><i class="fas fa-feather-alt"></i> Contact</a>
+				</li>
+				<li class="nav-item">
+					<a class="nav-link disabled" href="result.php"><i class="fas fa-dna"></i> Result</a>
+				</li>
+			</ul>
+		</nav>
+	</div>
 
 
-		<div class="topbar">
 		
-		<div class="menu">
-		
-			<div class="main-menu">
-				<a href="index.php" class="main-menu"><i class="fas fa-home"></i>Home</a>
-			</div>
-
-			<div class="main-menu">
-				<a href="about.php" class="main-menu"><i class="fas fa-info-circle"></i>About</a>
-			</div>
-
-			<div class="main-menu">
-				<a href="help.php" class="main-menu"><i class="fas fa-question-circle"></i>Help</a>
-			</div>
-
-			<div class="main-menu">
-				<a href="contact.php" class="main-menu"><i class="fas fa-feather-alt"></i>Contact</a>
-			</div>
-
-		</div>
-		</div>
-
-
-
-		<div class="content">
+		<div  class="container">
 			
-			<div>
-
-			<h2> Run predictions for your protein sequences :  </h2>
-
-	
-			<form action="process.php" method="post">
-			
-			<p>
-			<b>Job name</b> : <br>
-			<input type="jobname" name="jobname" placeholder="" id="jobname"> <br>
-
-			*between 3-20 alphanumerical characters and '_' (without spaces). 
-			<br>			
-
-			<br>
-			<b>Protein sequences</b> : <br>
-			<textarea wrap="hard" type="protseq" name="protseq" 
-
-			*FASTA format. 
-
-			placeholder=""
-
- 			id="protseq"></textarea> <br>
-
-			<br>
-			<br>
-
-			
-			<input type="submit" name = 'submit' value="Run"> 
-			
-			</p>
-			</form>
-
-
-			<br>
-			<br> 
-			<br>
-			<br>
-
-
+			<div class="content text-center">
+			<div class="logo">
+				<img src="../images/dnaushpii.png" alt="logo">
+			</div>
+				<h1 class="pb-5"> Run predictions for your protein sequences :  </h1>
+				<form action="process.php" method="post">
+					<div class="form-group">
+						<label for="jobname"><h3 class="text-uppercase">Job name:</h3></label>
+						<input type="jobname" class="form-control" placeholder="*Between 3-20 alphanumerical characters and '_' (without spaces)." id="jobname" name="jobname">
+					</div>
+					<div class="form-group">
+						<label for="protseq"><h3 class="text-uppercase">Protein sequences:</h3></label>
+						<textarea class="form-control" rows="10" id="protseq" type="protseq" name="protseq" wrap="hard" placeholder="*FASTA format"></textarea>
+					</div> 
+				<input type="submit" class="btn btn-primary text-uppercase" name = 'submit' value="Run Sequences"> 
+				</p>
+				</form>
 			</div>
 		</div>
 
--- results.php
+++ results.php
@@ -15,93 +15,85 @@ $OutputFolder = $_SESSION['OutputFolder'] ;
 		<meta charset="utf-8">
 		<title> Cross Species Workflow Web Server </title>
 		<link href="styles/style.css" rel="stylesheet" type="text/css">
-		<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+		<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
 		<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.1/css/all.css">
 	</head>
 
 
 	<body>
 
-		<div class="banner">
-			<img src="img/logo.png"></img>
-		</div>
 
-
-		<div class="topbar">
-		
-		<div class="menu">
-		
-			<div class="main-menu">
-				<a href="index.php" class="main-menu"><i class="fas fa-home"></i>Home</a>
-			</div>
-
-			<div class="main-menu">
-				<a href="about.php" class="main-menu"><i class="fas fa-info-circle"></i>About</a>
-			</div>
-
-			<div class="main-menu">
-				<a href="help.php" class="main-menu"><i class="fas fa-question-circle"></i>Help</a>
-			</div>
-
-			<div class="main-menu">
-				<a href="contact.php" class="main-menu"><i class="fas fa-feather-alt"></i>Contact</a>
-			</div>
-
-		</div>
-		</div>
-
-
-
-
-
-
-	<div class="content">
-
-	<h2>Results</h2>
-
-	<div>
-
-
-	<h3> Job name : </b> <?php echo $JobName; ?> </h3> 
-		
-		<div id="logs" >
-		Structural prediction results:  <br><br>
-
-		<table>
-
-<?php
-
-$outputFILE = fopen($OutputFolder . '/struct_results.tsv',"r") or die("Error");   
-                                                                                      
-while(($row = fgets($outputFILE)) != false) {     
-                                                                                                  
-		echo "<tr>";     
-        $col = explode("\t",$row);
-		$i = 0;
-
-        foreach($col as $data) {
-			echo "<td>". trim($data)."</td>";
-			$i++;
-		}                     
-        echo "</tr>";
-}
-
-?>
-
-		</table>
-
-
-
-</div>
-</div>
-</div>
-</div>
-
-
-	<div class="footer">
-	<br>
+	<div class="topbar">
+		<nav class="navbar navbar-expand-sm bg-primary navbar-dark fixed-top">
+			
+			<ul class="navbar-nav">
+				<li class="nav-item">
+					<a class="nav-link" href="index.php"><i class="fas fa-home"></i> Home</a>
+				</li>
+				<li class="nav-item">
+					<a class="nav-link" href="about.php"><i class="fas fa-info-circle"></i> About</a>
+				</li>
+				<li class="nav-item">
+					<a class="nav-link" href="help.php"><i class="fas fa-question-circle"></i> Help</a>
+				</li>
+				<li class="nav-item">
+					<a class="nav-link" href="contact.php"><i class="fas fa-feather-alt"></i> Contact</a>
+				</li>
+				<li class="nav-item">
+					<a class="nav-link active" href="result.php"><i class="fas fa-dna"></i> Result</a>
+				</li>
+			</ul>
+		</nav>
 	</div>
 
+
+
+
+
+
+
+	<div class="table">
+
+		
+		
+		<div class="logs" >
+			<h2>Results</h2>
+			<h3> Job name : </b> <?php echo $JobName; ?> </h3> 
+			<a href="index.php" class="btn btn-primary btn-lg active mb-3 mt-2 text-uppercase" role="button" aria-pressed="true">Start new job</a>
+			<p>Structural prediction results:</p>   
+			
+
+			<table class="table-sm table-dark table-striped">
+				<tbody id="tby1">
+					<?php
+
+					$outputFILE = fopen($OutputFolder . '/struct_results.tsv',"r") or die("Error");   
+																										
+					while(($row = fgets($outputFILE)) != false) {     
+																													
+							echo "<tr>";     
+							$col = explode("\t",$row);
+							$i = 0;
+
+							foreach($col as $data) {
+								echo "<td>". trim($data)."</td>";
+								$i++;
+							}                     
+							echo "</tr>";
+					}
+
+					?>
+				</tbody>
+			</table>
+
+
+
+		</div>
+	</div>
+	
 	</body>
 
 </html>
